bfafd7820ade4386c193e986301c8fe30aa6ed46
Utkarsh Kumar <kumar.utkarsh007@gmail.com>
2016-11-21 20:51:27 +0530

    all worked out but encountering a ValueError :(

--- loss.py
+++ loss.py
@@ -2,18 +2,6 @@ import cv2
 import numpy as np
 import math
 from matplotlib import pyplot as plt
-# def blur_uv_loss(rgb, inferred_rgb):
-#   uv = rgb2uv(rgb)
-#   uv_blur0 = rgb2uv(blur(rgb, 3))
-#   uv_blur1 = rgb2uv(blur(rgb, 5))
-
-#   inferred_uv = rgb2uv(inferred_rgb)
-#   inferred_uv_blur0 = rgb2uv(blur(inferred_rgb, 3))
-#   inferred_uv_blur1 = rgb2uv(blur(inferred_rgb, 5))
-
-#   return  ( dist(inferred_uv, uv) +
-#             dist(inferred_uv_blur0 , uv_blur0) +
-#             dist(inferred_uv_blur1, uv_blur1) ) / 3
 
 def get_UV(img):
   r = img.take((0,), axis = 2)
@@ -47,39 +35,3 @@ def convert_RGB(img):
 
 def blur(img, sigma):
   return cv2.GaussianBlur(img, (5, 5), sigma, sigma)
-
-def loss(actual_image, output):
-  actual_uv = get_UV(actual_image)
-  output_uv = get_UV(output)
-  diff1 = actual_uv - output_uv
-
-  blurred_actual_uv_3 = get_UV(blur(actual_image, 3));
-  blurred_output_uv_3 = get_UV(blur(output, 3));
-  diff2 = blurred_actual_uv_3 - blurred_output_uv_3
-
-  blurred_actual_uv_5 = get_UV(blur(actual_image, 5));
-  blurred_output_uv_5 = get_UV(blur(output, 5));
-  diff3 = blurred_actual_uv_5 - blurred_output_uv_5
-  
-  return (np.linalg.norm(diff1) + np.linalg.norm(diff2) + np.linalg.norm(diff3))/3
-
-
-# img = cv2.imread('boat.jpg')
-# output = blur(img, 4)
-
-# diff = transform_img - img;
-
-# plt.subplot(121),plt.imshow(img),plt.title('Original')
-# plt.xticks([]), plt.yticks([])
-# plt.subplot(122),plt.imshow(transform_img),plt.title('Blurred')
-# plt.xticks([]), plt.yticks([])
-# plt.show()
-# diff = transform_img - img;
-# print np.linalg.norm(diff)
-# print np.mean(transform_img.astype(int));
-# print np.mean(img);
-
-# b = get_UV(100, 2, 3);
-# print a;
-# print convert_RGB(b[0], b[1], b[2]);
-# print loss(img, output)
--- main.py
+++ main.py
@@ -63,25 +63,30 @@ rhs4 = tf.nn.relu(conv2d(r, W_conv4) + b_conv4)
 W_conv5 = weight_variable([3, 3, 3, 2])
 b_conv5 = bias_variable([2])
 
+# keep_prob = tf.placeholder(tf.float32)
+
 output = tf.nn.relu(conv2d(rhs4, W_conv5) + b_conv5)
 
-loss_to_min = loss(output, imgs_uv)
+loss = tf.reduce_sum(tf.squared_difference(output, imgs_uv))
 
-var = [vgg.W_conv0, vgg.b_conv0, vgg.W_conv1, vgg.b_conv1, vgg.W_conv2, vgg.b_conv2, vgg.W_conv3, vgg.b_conv3, vgg.W_conv4, vgg.b_conv4, vgg.W_conv5, vgg.b_conv5]
-opt = AdamOptimizer(1e-3)
-train_step = opt.minimize(loss, var)
+var = [W_conv0, b_conv0, W_conv1, b_conv1, W_conv2, b_conv2, W_conv3, b_conv3, W_conv4, b_conv4, W_conv5, b_conv5]
+opt = tf.train.AdamOptimizer(1e-3)
+train_step = opt.minimize(loss, var_list=var)
 
-init = tf.initilize_all_variables()
+init = tf.initialize_all_variables()
 sess.run(init)
 
 
-image_list = subprocess.check_output("ls -1 "+train_path, shell=True).split('\n')[:-1]
-rgb_data = np.array([imresize(imread(train_path+i, mode='RGB'), (image_px, image_px)) for i in image_list], dtype='float32')
+image_list = subprocess.check_output("ls -1 "+train_path+"*.jpg", shell=True).split('\n')[:-1]
+rgb_data = np.array([imresize(imread(i, mode='RGB'), (image_px, image_px)) for i in image_list], dtype='float32')
 rgb_data = rgb_data/255.0
+
 # np.save('rgb_data', rgb_data, allow_pickle=False)
 # np.load('rgb_data')
 
 y_data = np.array([rgb2gray(i) for i in rgb_data])
+y_data = np.reshape(y_data, [y_data.shape[0], y_data.shape[1], y_data.shape[2], 1])
+y_data_con = np.concatenate([y_data, y_data, y_data], axis=3)
 uv_data = np.array([get_UV(i) for i in rgb_data])
 # np.save('y_data', y_data, allow_pickle=False)
 # np.save('uv_data', uv_data, allow_pickle=False)
@@ -89,7 +94,7 @@ uv_data = np.array([get_UV(i) for i in rgb_data])
 # np.load('uv_data')
 
 
-train_accuracy = sess.run(loss, feed_dict={x: y_data, y_: uv_data, keep_prob: 0.5})
+train_accuracy = sess.run(loss, feed_dict={imgs: y_data_con[:10], imgs_uv: uv_data[:10]})
 print(train_accuracy)
 
 sess.run(train_step, feed_dict={x: batch_xs, y_: batch_ys, keep_prob: 0.5})
@@ -98,14 +103,17 @@ train_accuracy = sess.run(loss, feed_dict={x: y_data, y_: uv_data, keep_prob: 0.
 print(train_accuracy)
 
 
-# n_images = len(y_data)
-# for i in range(n_iters):
-# 	batch_xs = y_data[batch_size*(i%) : batch_size*(i%(n_labels+1)+1)]
-# 	batch_ys = uv_data[batch_size*(i%(n_labels+1)) : batch_size*(i%(n_labels+1)+1)]
-# 	if i%(n_labels+1) == 0:
-# 		shuffle_in_unison(train, train_labels)
-# 	if i%100 == 0:
-# 		train_accuracy = sess.run(accuracy, feed_dict={x: train[:n_train/100], y_: train_labels[:n_train/100], keep_prob: 0.5})
-# 		valid_accuracy = sess.run(accuracy, feed_dict={x: valid, y_: valid_labels, keep_prob: 0.5})
-# 		print("step %d, training accuracy %g validation accuracy %g"%(i, train_accuracy, valid_accuracy))	
-# 	sess.run(train_step, feed_dict={x: batch_xs, y_: batch_ys, keep_prob: 0.5})
+n_images = len(y_data)
+batch_size = 10
+n = n_images/batch_size = 100
+
+for i in range(n_iters):
+	batch_xs = y_data[batch_size*(i%n) : batch_size*(i%n+1)]
+	batch_ys = uv_data[batch_size*(i%n) : batch_size*(i%n+1)]
+	if i%n == 0:
+		shuffle_in_unison(train, train_labels)
+	if i%100 == 0:
+		train_accuracy = sess.run(accuracy, feed_dict={x: train[:n_train/100], y_: train_labels[:n_train/100], keep_prob: 0.5})
+		valid_accuracy = sess.run(accuracy, feed_dict={x: valid, y_: valid_labels, keep_prob: 0.5})
+		print("step %d, training accuracy %g validation accuracy %g"%(i, train_accuracy, valid_accuracy))	
+	sess.run(train_step, feed_dict={x: batch_xs, y_: batch_ys, keep_prob: 0.5})
